6b1aed81852723c6ab8194db155b95be5aa1b10b
Denis Vergnes <dvergnes@splunk.com>
2022-01-09 12:01:15 -0800

    Add config

--- cert-monitor/monitor/config.go
+++ cert-monitor/monitor/config.go
@@ -1,1 +1,14 @@
 package monitor
+
+import "time"
+
+type Scrapping struct {
+	Endpoint string `yaml:"endpoint"`
+	Metric   string `yaml:"metric"`
+	Timeout time.Duration `yaml:"timeout"`
+}
+
+type Config struct {
+	Threshold time.Duration `yaml:"threshold"`
+	Scrapping Scrapping `yaml:"scrapping"`
+}
