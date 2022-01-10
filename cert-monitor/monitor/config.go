d429a86b500d7e46960b9f419dcb9852e78f4afc
Denis Vergnes <dvergnes@splunk.com>
2022-01-09 20:32:59 -0800

    Add doc

--- cert-monitor/monitor/config.go
+++ cert-monitor/monitor/config.go
@@ -2,12 +2,20 @@ package monitor
 
 import "time"
 
+// Config contains the configuration for the monitor
 type Config struct {
+	// Threshold defines the duration for which a certificate is not considered as close to expiration. For example,
+	// if a certificate is valid for the next 20 days but the threshold is set to 30 days, it is considered as close to
+	// expiration.
 	Threshold time.Duration `yaml:"threshold"`
+	// GathererConfig contains the configuration for fetching the certificate info
 	GathererConfig GathererConfig `yaml:"gatherer"`
 }
 
+// GathererConfig contains the configuration for fetching the certificate info
 type GathererConfig struct {
+	// PageSize defines the page size when calling the list certificate API
 	PageSize int64          `yaml:"page_size"`
+	// Timeout defines the timeout to fetch a page
 	Timeout  time.Duration `yaml:"timeout"`
 }
